a96d53d9a2cad458b357729d564a15ff6a012000
erikaCutugno <erikucciapop@gmail.com>
2025-03-12 20:21:51 +0100

    add singol page and fix file env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_BE_URL,
-    timeout: 5000,
-})
+  baseURL: import.meta.env.VITE_BE_URL,
+  timeout: 5000,
+});
 
-export default instance
+export default instance;
