3934b6d8d39ca12c3f95719c4df8903a66e9e5e7
Alessandro Frisone <alefri2004@gmail.com>
2025-03-13 15:14:10 +0100

    feat: add style to single product page

--- src/components/ui/CardSinglePage.jsx
+++ src/components/ui/CardSinglePage.jsx
@@ -15,7 +15,7 @@ export default function CardSinglePage({
   year_edition,
   isbn,
   pages_number,
-  description
+  description,
 }) {
   return (
     <div className="product-page">
@@ -23,47 +23,47 @@ export default function CardSinglePage({
         <div className="product-image">
           <img src={image} alt={title} />
         </div>
-
         <div className="product-details">
           <h1 className="product-title">{title}</h1>
           <h2 className="product-author">{author}</h2>
-
-          <p className="product-info">
-            Genere: <span>{category}</span>
-          </p>
-          <p className="product-info">
-            Anno di pubblicazione: <span>{year_of_release}</span>
-          </p>
-          <p className="product-info">
-            Anno di edizione: <span>{year_edition}</span>
-          </p>
-          <p className="product-info">
-            Formato: <span>{format}</span>
-          </p>
-          <p className="product-info">
-            ISBN: <span>{isbn}</span>
-          </p>
-          <p className="product-info">
-            Numero di pagine: <span>{pages_number}</span>
-          </p>
-
-          <p className="product-description">Descrizione: <span>{description}</span></p>
-
+          <div className="box-detail">
+            <span className="product-info">
+              Genere: <p>{category}</p>
+            </span>
+            <span className="product-info">
+              Anno di pubblicazione: <p>{year_of_release}</p>
+            </span>
+            <span className="product-info">
+              Anno di edizione: <p>{year_edition}</p>
+            </span>
+            <span className="product-info">
+              Formato: <p>{format}</p>
+            </span>
+            <span className="product-info">
+              ISBN: <p>{isbn}</p>
+            </span>
+            <span className="product-info">
+              Numero di pagine: <p>{pages_number}</p>
+            </span>
+            <span className="product-info">
+              Descrizione: <p>{description}</p>
+            </span>
+          </div>
           {discountId &&
           new Date() >= new Date(start_date) &&
           new Date() <= new Date(end_date) ? (
             discount_type === "percentage" ? (
               <div className="discount-container">
-                <span>{discountDescription}</span>
                 <span className="old-price">{price}€</span>
+                <span>{discountDescription}</span>
                 <span className="new-price">
                   {(price - (price * value) / 100).toFixed(2)}€
                 </span>
               </div>
             ) : (
               <div className="discount-container">
-                <span>{discountDescription}</span>
                 <span className="old-price">{price}€</span>
+                <span>{discountDescription}</span>
                 <span className="new-price">{value}€</span>
               </div>
             )
